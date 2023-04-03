6b7dc12fdab49b1d52398b8603db9032344c7452
DanielleSmith0 <Danielahsmith@icloud.com>
2023-04-03 14:41:49 -0600

    initial commit

---
+++ tripTime.js
@@ -0,0 +1,110 @@
+const tripTime = (arr) => {
+    let result = {
+        avgSpeedLimits: [], 
+        segmentTimes: [], 
+        totalTime: null
+    }
+
+
+    return result
+}
+
+// UNCOMMENT THE FUNCTION CALL AT
+// THE BOTTOM BEFORE RUNNING THE FILE
+
+//Segement time = distance divided by speed limit -> round decimal.
+//Push to segmentTimes array.
+//Total time is all of the segment times added up, so we can say it is the result of adding the numbers of the array. Reduce method.
+
+
+
+const stops = [
+    {
+        name: `Gus's Gas`, 
+        speedLimits: [
+            {
+                distance: 5,
+                speedLimit: 45
+            },
+            {
+                distance: 97,
+                speedLimit: 65
+            },
+            {
+                distance: 72,
+                speedLimit: 70
+            },
+            {
+                distance: 25,
+                speedLimit: 50
+            }
+        ], 
+        traffic: 12
+    }, 
+    {
+        name: `Halle's House of Pancakes`, 
+        speedLimits: [
+            {
+                distance: 36,
+                speedLimit: 50
+            },
+            {
+                distance: 141,
+                speedLimit: 75
+            }
+        ], 
+        traffic: 0
+    }, 
+    {
+        name: `Jake's Great Shakes`, 
+        speedLimits: [
+            {
+                distance: 100,
+                speedLimit: 75
+            },
+            {
+                distance: 84,
+                speedLimit: 70
+            },
+            {
+                distance: 20,
+                speedLimit: 75
+            }
+        ], 
+        traffic: 30
+    }, 
+    {
+        name: `Luna's Lunch Counter`, 
+        speedLimits: [
+            {
+                distance: 3,
+                speedLimit: 35
+            },
+            {
+                distance: 5,
+                speedLimit: 45
+            },
+            {
+                distance: 20,
+                speedLimit: 65
+            },
+            {
+                distance: 85,
+                speedLimit: 75
+            },
+            {
+                distance: 3,
+                speedLimit: 65
+            },
+            {
+                distance: 5,
+                speedLimit: 55
+            }
+        ], 
+        traffic: 7
+    }, 
+
+]
+
+
+// tripTime(stops)
